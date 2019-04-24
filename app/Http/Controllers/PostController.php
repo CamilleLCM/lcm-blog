ea93eedb7957eb665e2a34d18d70196ce5e4d03c
liangchunmei <942859005@qq.com>
2019-04-24 17:01:40 +0800

    '第一次提交'

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //文章列表
+    public function index(){
+        $posts = [
+            [
+                'title'=>'this is title1'
+            ],
+            [
+                'title'=>'this is title2'
+            ],
+            [
+                'title'=>'this is title3'
+            ]
+        ];
+        return view('post/index',compact('posts'));
+    }
+    //文章详情
+    public function show(){
+        return view('post/show',['title'=>'this is title'],['isShow'=>false]);
+    }
+    //创建页面
+    public function create(){
+
+        return view('post/create');
+    }
+    //创建逻辑
+    public function store(){
+
+    }
+    //编辑页面
+    public function edit(){
+        return view('post/edit');
+    }
+    //编辑逻辑
+    public function update(){
+
+    }
+    //删除逻辑
+    public function delete(){
+
+    }
+}
